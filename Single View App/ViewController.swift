753e8b6893d26ff78dc6156d0e6afa80581b11d8
Vahan Asryan <vahan.asryan@avetiq.de>
2019-04-03 19:15:07 +0400

    Initial Commit

---
+++ Single View App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Single View App
+//
+//  Created by Vahan Asryan on 4/3/19.
+//  Copyright © 2019 Vahan Asryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
